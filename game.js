cbb9607f3336793e7491f0f2ef273c0389afd143
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2021-11-06 12:43:33 +0100

    change budler to esbuild

--- game.js
+++ game.js
@@ -2,4 +2,40 @@ import kaboom from "kaboom";
 
 kaboom();
 
-add([text("Rama"), pos(120, 80)]);
+loadSprite("mario", "./sprites/mario.png");
+
+scene("game", () => {
+  //   const level = [
+  //     "          ",
+  //     "          ",
+  //     "          ",
+  //     "          ",
+  //     "          ",
+  //     "          ",
+  //     "          ",
+  //   ];
+
+  const mario = add([
+    sprite("mario"),
+    pos(width() / 2, height() / 2),
+    scale(0.1),
+    origin("center"),
+    // body(),
+    area(),
+    "mario",
+  ]);
+
+  keyDown("right", () => {
+    mario.move(200, 0);
+  });
+
+  keyDown("left", () => {
+    mario.move(-200, 0);
+  });
+
+  keyDown("space", () => {
+    mario.jump();
+  });
+});
+
+go("game");
