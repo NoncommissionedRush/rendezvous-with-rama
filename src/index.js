c65274222ef6b675fcf0ac076f96f6a71e092804
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2021-11-06 16:15:04 +0100

    update animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import kaboom from 'kaboom';
+import kaboom from "kaboom";
 
 kaboom({
   background: [0, 0, 255],
@@ -6,20 +6,21 @@ kaboom({
 
 let isJumping;
 
-loadSprite('mario', './sprites/mario.png');
-loadSprite('ground2', './sprites/ground2.png');
-loadSprite('crab', './sprites/crab.png');
-loadSprite('ladder', './sprites/ladder.png');
-loadSpriteAtlas('./sprites/run.png', {
+loadSprite("ground2", "./sprites/ground2.png");
+loadSprite("crab", "./sprites/crab.png");
+loadSprite("ladder", "./sprites/ladder.png");
+loadSpriteAtlas("./sprites/run.png", {
   player: {
     x: 0,
     y: 0,
-    width: 600,
-    height: 250,
-    sliceX: 6,
+    width: 900,
+    height: 160,
+    sliceX: 9,
     anims: {
-      'run-side': { from: 0, to: 5, loop: true, speed: 15 },
+      "run-side": { from: 1, to: 6, loop: true, speed: 15 },
       idle: { from: 0, to: 0 },
+      jump: { from: 7, to: 7 },
+      "power-jump": { from: 8, to: 8 },
     },
   },
 });
@@ -28,111 +29,132 @@ loadSpriteAtlas('./sprites/run.png', {
 const SPEED = 220;
 const JUMP_STRENGTH = height() / 1.3;
 
-scene('game', () => {
+scene("game", () => {
   const levelLayout = [
-    '                                        ',
-    '                                        ',
-    '                                        ',
-    '                                        ',
-    '                ###                     ',
-    '                                        ',
-    '                                        ',
-    '######                   ##             ',
-    '                                        ',
-    '                                        ',
-    '                                        ',
-    '                   #######              ',
-    '                                        ',
-    '                                        ',
-    '                                        ',
-    '           ###                          ',
-    '                                        ',
-    '                                        ',
-    '                                E       ',
-    '####################################### ',
+    "                                        ",
+    "                                        ",
+    "                                        ",
+    "                                        ",
+    "                ###                     ",
+    "                                        ",
+    "                                        ",
+    "######                   ##             ",
+    "                                        ",
+    "                                        ",
+    "                                        ",
+    "                   #######              ",
+    "                                        ",
+    "                                        ",
+    "          ###                           ",
+    "                                        ",
+    "                                        ",
+    "                                        ",
+    "                                E       ",
+    "####################################### ",
   ];
 
   addLevel(levelLayout, {
     width: width() / 40,
     height: height() / 20,
-    '#': () => [
-      sprite('ground2'),
+    "#": () => [
+      sprite("ground2"),
       scale(0.2),
       area(0.5),
       solid(),
-      origin('topleft'),
-      'ground',
+      origin("topleft"),
+      "ground",
     ],
     E: () => [
-      sprite('crab'),
+      sprite("crab"),
       scale(0.2),
       area(),
       solid(),
       body(),
-      origin('bot'),
-      'enemy',
+      origin("bot"),
+      "enemy",
     ],
   });
 
-  const mario = add([
-    sprite('player'),
+  const player = add([
+    sprite("player"),
     pos(width() / 2, height() / 2),
     scale(0.5),
-    origin('center'),
+    origin("center"),
     body(),
-    area(),
-    'player',
+    area({ height: 160 }),
+    "player",
   ]);
 
-  mario.play('idle');
+  player.play("idle");
 
-  keyDown('right', () => {
-    if (mario.curAnim() !== 'run-side') {
-      mario.play('run-side');
+  keyDown("right", () => {
+    if (player.curAnim() !== "run-side" && player.isGrounded()) {
+      player.play("run-side");
     }
-    mario.flipX(false);
-    mario.move(SPEED, 0);
+    player.flipX(false);
+    player.move(SPEED, 0);
   });
 
-  keyRelease('right', () => {
-    mario.play('idle');
+  keyRelease("right", () => {
+    player.play("idle");
   });
 
-  keyDown('left', () => {
-    if (mario.curAnim() !== 'run-side') {
-      mario.play('run-side');
+  keyDown("left", () => {
+    if (player.curAnim() !== "run-side" && player.isGrounded()) {
+      player.play("run-side");
     }
-    mario.flipX(true);
-    mario.move(-SPEED, 0);
+    player.flipX(true);
+    player.move(-SPEED, 0);
   });
 
-  keyRelease('left', () => {
-    mario.play('idle');
-    mario.flipX(true);
+  keyRelease("left", () => {
+    player.play("idle");
+    player.flipX(true);
   });
 
-  keyDown('space', () => {
-    if (mario.isGrounded()) {
-      mario.jump(JUMP_STRENGTH);
+  keyDown("space", () => {
+    if (player.isGrounded()) {
+      player.jump(JUMP_STRENGTH);
     }
+    player.play("jump");
+    isJumping = true;
+  });
+
+  keyDown("m", () => {
+    if (player.isGrounded()) {
+      player.jump(JUMP_STRENGTH * 1.5);
+    }
+    player.play("power-jump");
     isJumping = true;
   });
 
   // enemy collisions
-  action('enemy', (e) => {
+  action("enemy", (e) => {
     e.move(-200, 0);
     cleanup();
   });
 
-  mario.collides('enemy', (e) => {
+  // action("player", (player) => {
+  //   if (!player.isGrounded()) {
+  //     player.play("jump");
+  //   }
+  // });
+
+  player.collides("enemy", (e) => {
     if (isJumping) {
       destroy(e);
       shake(5);
     } else {
-      destroy(mario);
+      destroy(player);
       shake(5);
+    }
+  });
+
+  player.collides("ground", () => {
+    if (player.curAnim() !== "run-side") {
+      player.play("idle");
     }
   });
 });
 
-go('game');
+go("game");
