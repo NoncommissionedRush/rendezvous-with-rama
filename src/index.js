33a0c1667693b67fc22e3468a7e505b32036c9bb
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2021-11-06 20:04:15 +0100

    add new levels

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import kaboom from 'kaboom';
-import levelOneLayout, { levelThreeLayout, levelTwoLayout } from './levels';
+import kaboom from "kaboom";
+import levelOneLayout, { levelThreeLayout, levelTwoLayout } from "./levels";
 
 kaboom({
   background: [0, 0, 255],
 });
 
-loadSprite('bg', './sprites/bg1.png');
-loadSprite('ground-blue', './sprites/Ground_blue.png');
-loadSprite('ground-pink', './sprites/Ground_pink.png');
-loadSprite('ground-purple', './sprites/Ground_purple.png');
-loadSprite('ground-yellow', './sprites/Ground_yellow.png');
-loadSprite('collect', './sprites/collect.png');
-loadSpriteAtlas('./sprites/run.png', {
+loadSprite("bg1", "./sprites/bg1.png");
+loadSprite("bg2", "./sprites/bg2.png");
+loadSprite("bg3", "./sprites/bg3.png");
+loadSprite("ground-blue", "./sprites/Ground_blue.png");
+loadSprite("ground-pink", "./sprites/Ground_pink.png");
+loadSprite("ground-purple", "./sprites/Ground_purple.png");
+loadSprite("ground-yellow", "./sprites/Ground_yellow.png");
+loadSprite("collect", "./sprites/collect.png");
+loadSpriteAtlas("./sprites/run.png", {
   player: {
     x: 0,
     y: 0,
@@ -19,14 +21,14 @@ loadSpriteAtlas('./sprites/run.png', {
     height: 160,
     sliceX: 9,
     anims: {
-      'run-side': { from: 1, to: 6, loop: true, speed: 15 },
+      "run-side": { from: 1, to: 6, loop: true, speed: 15 },
       idle: { from: 0, to: 0 },
       jump: { from: 7, to: 7 },
-      'power-jump': { from: 8, to: 8 },
+      "power-jump": { from: 8, to: 8 },
     },
   },
 });
-loadSpriteAtlas('./sprites/crab1.png', {
+loadSpriteAtlas("./sprites/crab1.png", {
   crab: {
     x: 0,
     y: 0,
@@ -43,68 +45,73 @@ loadSpriteAtlas('./sprites/crab1.png', {
 const SPEED = 220;
 const JUMP_STRENGTH = height() / 1.3;
 
-scene('game', (level = 2) => {
+scene("game", (level = 1, scoreValue = 0) => {
   // ADD BACKROUND
-  add([sprite('bg', { width: width(), height: height() }), fixed()]);
 
   let levelLayout;
 
   if (level === 1) {
     levelLayout = levelOneLayout;
+    add([sprite("bg1", { width: width(), height: height() }), fixed()]);
   } else if (level === 2) {
+    add([sprite("bg2", { width: width(), height: height() }), fixed()]);
     levelLayout = levelTwoLayout;
   } else if (level === 3) {
+    add([sprite("bg3", { width: width(), height: height() }), fixed()]);
     levelLayout = levelThreeLayout;
   }
 
   addLevel(levelLayout, {
     width: width() / 20,
     height: height() / 20,
-    '#': () => [
-      sprite('ground-purple'),
+    "#": () => [
+      sprite("ground-purple"),
       scale(0.3),
       area(0.5),
       solid(),
-      origin('topleft'),
-      'ground',
+      origin("topleft"),
+      "ground",
     ],
 
     E: () => [
-      sprite('crab'),
+      sprite("crab"),
       scale(0.2),
       area(),
       solid(),
       body(),
       pos(),
-      origin('bot'),
+      origin("bot"),
       { speed: 200 },
-      'enemy',
+      "enemy",
     ],
     $: () => [
-      sprite('collect'),
+      sprite("collect"),
       scale(0.2),
       area(),
       solid(),
       rotate(0),
-      //body(),
-      //pos(),
-      origin('center'),
-      'collect',
+      origin("center"),
+      "collect",
     ],
   });
 
   const player = add([
-    sprite('player'),
+    sprite("player"),
     pos(width() / 2, height() / 2),
     scale(0.5),
-    origin('center'),
+    origin("center"),
     body(),
     area({ height: 160 }),
-    'player',
+    "player",
   ]);
 
   // score
-  const score = add([text('Score: 0'), pos(12, 120), { value: 0 }, fixed()]);
+  const score = add([
+    text(`Score: ${scoreValue}`),
+    pos(12, 120),
+    { value: scoreValue },
+    fixed(),
+  ]);
 
   // countdown
   let ttt = 2;
@@ -115,54 +122,54 @@ scene('game', (level = 2) => {
     ttt = ttt - dt();
     timer.text = ttt.toFixed(2);
     if (ttt <= 0) {
-      timer.text = 'Game over!';
+      timer.text = "Game over!";
     }
   });
 
-  player.play('idle');
+  player.play("idle");
 
-  keyDown('right', () => {
-    if (player.curAnim() !== 'run-side' && player.isGrounded()) {
-      player.play('run-side');
+  keyDown("right", () => {
+    if (player.curAnim() !== "run-side" && player.isGrounded()) {
+      player.play("run-side");
     }
     player.flipX(false);
     player.move(SPEED, 0);
   });
 
-  keyRelease('right', () => {
-    player.play('idle');
+  keyRelease("right", () => {
+    player.play("idle");
   });
 
-  keyDown('left', () => {
-    if (player.curAnim() !== 'run-side' && player.isGrounded()) {
-      player.play('run-side');
+  keyDown("left", () => {
+    if (player.curAnim() !== "run-side" && player.isGrounded()) {
+      player.play("run-side");
     }
     player.flipX(true);
     player.move(-SPEED, 0);
   });
 
-  keyRelease('left', () => {
-    player.play('idle');
+  keyRelease("left", () => {
+    player.play("idle");
     player.flipX(true);
   });
 
-  keyDown('space', () => {
+  keyDown("space", () => {
     if (player.isGrounded()) {
       player.jump(JUMP_STRENGTH);
     }
-    player.play('jump');
+    player.play("jump");
   });
 
-  keyDown('m', () => {
+  keyDown("m", () => {
     if (player.isGrounded()) {
       player.jump(JUMP_STRENGTH * 1.5);
     }
-    player.play('power-jump');
+    player.play("power-jump");
   });
 
-  action('enemy', (e) => {
-    if (e.curAnim() !== 'walk') {
-      e.play('walk');
+  action("enemy", (e) => {
+    if (e.curAnim() !== "walk") {
+      e.play("walk");
     }
     e.move(e.speed, 0);
 
@@ -173,7 +180,7 @@ scene('game', (level = 2) => {
     }
   });
 
-  onCollide('enemy', 'enemy', (e1, e2) => {
+  onCollide("enemy", "enemy", (e1, e2) => {
     if (e1.speed === 200) {
       e1.speed = -200;
     }
@@ -181,7 +188,7 @@ scene('game', (level = 2) => {
     e2.speed = -e2.speed;
   });
 
-  player.collides('enemy', (e) => {
+  player.collides("enemy", (e) => {
     if (!player.isGrounded()) {
       destroy(e);
       shake(5);
@@ -191,37 +198,42 @@ scene('game', (level = 2) => {
     }
   });
 
-  player.collides('ground', () => {
-    if (player.curAnim() !== 'run-side') {
-      player.play('idle');
+  player.collides("ground", () => {
+    if (player.curAnim() !== "run-side") {
+      player.play("idle");
     }
   });
-  action('player', () => {
+  action("player", () => {
     var currCam = camPos();
-    if (currCam.x < player.pos.x) {
+    if (currCam.x < player.pos.x && currCam.x < 4200) {
       camPos(player.pos.x, currCam.y);
     }
     if (currCam.x > player.pos.x && currCam.x > width() / 2) {
       camPos(player.pos.x, currCam.y);
     }
-  });
 
-  player.collides('ground', () => {
-    if (player.curAnim() !== 'run-side') {
-      player.play('idle');
+    if (player.pos.x > 4930) {
+      const newLevel = (level += 1);
+      go("game", newLevel, score.value);
     }
   });
 
-  player.collides('collect', (c) => {
+  player.collides("ground", () => {
+    if (player.curAnim() !== "run-side") {
+      player.play("idle");
+    }
+  });
+
+  player.collides("collect", (c) => {
     destroy(c);
     shake(1);
     score.value += 1;
-    score.text = 'Score:' + score.value;
+    score.text = "Score:" + score.value;
   });
 
-  action('collect', (c) => {
+  action("collect", (c) => {
     c.angle += 1;
   });
 });
 
-go('game');
+go("game");
